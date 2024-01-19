48d0c2e934a7515253fc55e03acf2564fb34381b
tornike-topuria <147945276+tornike-topuria@users.noreply.github.com>
2024-01-19 18:07:20 +0400

    later

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ for(let i = 0; i <= 95; i = i + 1) {
 }
 
 // 2
-// let array1= [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
-// for( i > 0; i < 10; i = i + 1){
-//     console.log(1 + i)
+let array1= [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+for( array1 > 0; array1 < 10; array1 = array1 + 1){
+    console.log( 1 + array1)
+    console.log(array1 [0, 1, 2, 5, 6, 7, 9, 10])
+   }
 
 // 3
-// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// for(array2 > 0; array2 < 10; array2 = array2 + 1){
-//    console.log(array2[4] )
+ let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];{
+    for(array2 > 0; array2  < 10; array2 = array2 + 1)
+    console.log(array2[4] + "არის" )
+    if(array2){console.log("არის")}
+ }
 
  // 4  
 let array3 = [1, 2, 3, 4, 5];
@@ -25,14 +29,11 @@ console.log(sum);
 
 // 5
 let array4 = [1, 2, 3, 7, 6, 9];
-
-let sum2 = 0;
-
 for (let average = 0; average < array4.length; average++) {
-    sum2 = sum2 + array4[average]
+    sum = sum + array4[average]
 }
 
-console.log(sum2 / array4.length);
+console.log(sum / array4.length);
 
  
 // 6
@@ -48,8 +49,7 @@ let user = {
    lastname: "smith",
    age: 25,
    studentstatus: "active"
-};
-
+}
 console.log(user.studentstatus);
 
 // 8
@@ -58,7 +58,6 @@ let user2 = {
    age: 20,
    studentstatus: 'active'
 }
-
 if (user2.age < 18 && user2.studentstatus == "active") {
    console.log("hello");
 }
@@ -82,10 +81,10 @@ else {
 
 
 // 9
-let array = [ [2, -3, 5, 10], [25, -24, -11, 100], [-6, -7, 10] ];
-for(array = 0; array <= 10; array = array + 1){
-   console.log(array[[0[0,2,3], [0,3],[2]]])
-}
+// let array = [ [2, -3, 5, 10], [25, -24, -11, 100], [-6, -7, 10] ];
+// for(array = 0; array <= 10; array = array + 1){
+//    console.log(array[[0[0,2,3], [0,3],[2]]])
+// }
 
 // 11
 let users = [
